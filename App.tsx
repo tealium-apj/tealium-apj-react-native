521699f4f54549c0eacb37c9f4cb7473db07b837
Ram in <raminhafezi@users.noreply.github.com>
2025-10-21 17:28:48 +1100

    consentStatus.consent

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { SafeAreaView, Button, Text } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, Button, Text} from 'react-native';
 import Tealium from 'tealium-react-native';
 import {
   TealiumConfig,
@@ -17,32 +17,34 @@ const App = () => {
   useEffect(() => {
     const config: TealiumConfig = {
       account: 'success-ramin-hafezi',
-      profile: 'mobile-test',
+      profile: 'main',
       environment: TealiumEnvironment.prod,
       collectors: [
         Collectors.AppData,
         Collectors.DeviceData,
-        Collectors.Connectivity
+        Collectors.Connectivity,
       ],
-      dispatchers: [Dispatchers.Collect],
-      // consentPolicy: ConsentPolicy.gdpr, // Remove this and the payload will send
+      dispatchers: [Dispatchers.Collect, Dispatchers.TagManagement],
+      consentPolicy: ConsentPolicy.gdpr, // Remove this and the payload will send
       visitorServiceEnabled: true,
     };
 
     Tealium.initialize(config);
-    Tealium.setConsentStatus(ConsentStatus.consented);
+    Tealium.joinTrace('PjiWBxzv');
     console.log('Tealium initialized');
   }, []);
 
   const trackTealiumEvent = () => {
     console.log('Sending Tealium event');
     try {
-      let tealEvent = new TealiumEvent(
-        'ButtonClicked',
-        { button_name: 'SignUp' }
-      );
+      let tealEvent = new TealiumEvent('ButtonClicked', {
+        button_name: 'SignUp',
+      });
+      Tealium.setConsentStatus(ConsentStatus.consented); // Remove this as well if you're not using consent
       Tealium.track(tealEvent);
-      setResult('Tracked event: ButtonClicked /' + JSON.stringify(tealEvent));
+      // Tealium.getConsentStatus(status => {
+      //   setResult('Tracked event: ButtonClicked /' + status);
+      // });
     } catch (e) {
       setResult('Error tracking event');
       console.error('Error tracking Tealium event:', e);
